d4e3a688db36179846c8bdb460126558053c917a
Matt <mattkeegan@btinternet.com>
2018-01-20 15:49:34 +0000

    update index

--- client/js/index.js
+++ client/js/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../app/component/App';
 
+import '../css/style.scss';
+
 ReactDOM.render(
     <App />,
     document.getElementById('application')
